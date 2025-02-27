78c4e9392d78cf020ed9a8c05dc3af89b4f74d1d
Matthias Kestenholz <mk@feinheit.ch>
2025-02-27 10:30:38 +0100

    Add some importmap merging tests

--- tests/testapp/test_media.py
+++ tests/testapp/test_media.py
@@ -38,3 +38,40 @@ class MediaTest(TestCase):
             str(merged),
             str(extended_a + media_ab + media_ac + extended_b + media_bc),
         )
+
+    def test_merging(self):
+        a = ImportMap(
+            {
+                "imports": {"a": "/static/a.js"},
+                "integrity": {"/static/a.js": "sha384-blub-a"},
+                "_unknown_": "Automatically dropped when merging.",
+            }
+        )
+        b = ImportMap(
+            {
+                "imports": {"b": "/static/b.js"},
+                "integrity": {"/static/b.js": "sha384-blub-b"},
+            }
+        )
+
+        self.assertEqual(
+            str(a + b),
+            """\
+<script type="importmap">{"imports": {"a": "/static/a.js", "b": "/static/b.js"}, "integrity": {"/static/a.js": "sha384-blub-a", "/static/b.js": "sha384-blub-b"}}</script>""",
+        )
+
+        c = ImportMap(
+            {
+                "imports": {
+                    "/app/": "./original-app/",
+                    "/app/helper": "./helper/index.mjs",
+                },
+                "scopes": {"/js": {"/app/": "./js-app/"}},
+            }
+        )
+
+        self.assertEqual(
+            str(a + b + c),
+            """\
+<script type="importmap">{"imports": {"a": "/static/a.js", "b": "/static/b.js", "/app/": "./original-app/", "/app/helper": "./helper/index.mjs"}, "integrity": {"/static/a.js": "sha384-blub-a", "/static/b.js": "sha384-blub-b"}, "scopes": {"/js": {"/app/": "./js-app/"}}}</script>""",
+        )
